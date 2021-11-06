15c6235a2c594279ceb27b9eda1694602a3d9a6b
ekiauhce <ekiauhce@yandex.ru>
2021-11-06 16:37:01 +0300

    Refactoring: easy add other sortings

--- Plot.java
+++ Plot.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.stream.DoubleStream;
 
 import javax.swing.*;
@@ -46,102 +49,52 @@ public class Plot extends JFrame {
         }
     }
     
+    private JTextField minNField;
+    private JTextField maxNField;
+    private JTextField deltaNField;
+    private JTextField itersField;
+
+    private JPanel buttonsPanel;
+    private JPanel chartPanel;
+
     private void createUI() {
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        JPanel chartPanel = new XChartPanel<XYChart>(chart);
+        chartPanel = new XChartPanel<XYChart>(chart);
         add(chartPanel);
 
         JPanel fieldsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        add(fieldsPanel);
+
         JLabel minNLabel = new JLabel("Min N:");
-        JTextField minNField = new JTextField("1000", 10);
+        minNField = new JTextField("1000", 10);
         minNLabel.setLabelFor(minNField);
         fieldsPanel.add(minNLabel);
         fieldsPanel.add(minNField);
-        add(fieldsPanel);
 
         JLabel maxNLabel = new JLabel("Max N:");
-        JTextField maxNField = new JTextField("15000", 10);
+        maxNField = new JTextField("15000", 10);
         maxNLabel.setLabelFor(maxNField);
         fieldsPanel.add(maxNLabel);
         fieldsPanel.add(maxNField);
-        add(fieldsPanel);
         
         JLabel deltaNLabel = new JLabel("Delta N:");
-        JTextField deltaNField = new JTextField("500", 10);
+        deltaNField = new JTextField("500", 10);
         deltaNLabel.setLabelFor(deltaNField);
         fieldsPanel.add(deltaNLabel);
         fieldsPanel.add(deltaNField);
-        add(fieldsPanel);
 
         JLabel itersLabel = new JLabel("Iters:");
-        JTextField itersField = new JTextField("5", 10);
+        itersField = new JTextField("5", 10);
         itersLabel.setLabelFor(itersField);
         fieldsPanel.add(itersLabel);
         fieldsPanel.add(itersField);
-        add(fieldsPanel);
 
-        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         
-        JButton jb1 = new JButton("selection");
-        jb1.addActionListener(e -> {
-            Benchmarking bench;
-            try {
-                bench = new Benchmarking(Selection::sort, jb1.getText(),
-                    Integer.parseInt(minNField.getText()),
-                    Integer.parseInt(maxNField.getText()),
-                    Integer.parseInt(deltaNField.getText()),
-                    Integer.parseInt(itersField.getText())
-                );
-            } catch (RuntimeException ex) {
-                bench = new Benchmarking(Selection::sort, jb1.getText());
-            }
-            bench.run();
-            
-            updateSeries(jb1.getText());
-            chartPanel.repaint();
-        });
-        buttonsPanel.add(jb1);
-        
-        JButton jb2 = new JButton("quick");
-        jb2.addActionListener(e -> {
-            Benchmarking bench;
-            try {
-                bench = new Benchmarking(Quick::sort, jb2.getText(),
-                    Integer.parseInt(minNField.getText()),
-                    Integer.parseInt(maxNField.getText()),
-                    Integer.parseInt(deltaNField.getText()),
-                    Integer.parseInt(itersField.getText())
-                );
-            } catch (RuntimeException ex) {
-                bench = new Benchmarking(Quick::sort, jb2.getText());
-            }
-            bench.run();
-
-            updateSeries(jb2.getText());
-            chartPanel.repaint();
-        });
-        buttonsPanel.add(jb2);
-    
-        JButton jb3 = new JButton("counting");
-        jb3.addActionListener(e -> {
-            Benchmarking bench;
-            try {
-                bench = new Benchmarking(Counting::sort, jb3.getText(),
-                    Integer.parseInt(minNField.getText()),
-                    Integer.parseInt(maxNField.getText()),
-                    Integer.parseInt(deltaNField.getText()),
-                    Integer.parseInt(itersField.getText())
-                );
-            } catch (RuntimeException ex) {
-                bench = new Benchmarking(Quick::sort, jb3.getText());
-            }
-            bench.run();
-
-            updateSeries(jb3.getText());
-            chartPanel.repaint();
-        });
-        buttonsPanel.add(jb3);
+        createSortingButton("selection");
+        createSortingButton("quick");
+        createSortingButton("counting");
 
         add(buttonsPanel);
         
@@ -150,6 +103,35 @@ public class Plot extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     
+    private static Map<String, Consumer<int[]>> nameToMethod = new HashMap<>();
+    static {
+        nameToMethod.put("selection", Selection::sort);
+        nameToMethod.put("counting", Counting::sort);
+        nameToMethod.put("quick", Quick::sort);
+    }
+
+    private void createSortingButton(String name) {
+        JButton jb = new JButton(name);
+        jb.addActionListener(e -> {
+            Benchmarking bench;
+            try {
+                bench = new Benchmarking(nameToMethod.get(name), jb.getText(),
+                    Integer.parseInt(minNField.getText()),
+                    Integer.parseInt(maxNField.getText()),
+                    Integer.parseInt(deltaNField.getText()),
+                    Integer.parseInt(itersField.getText())
+                );
+            } catch (RuntimeException ex) {
+                bench = new Benchmarking(nameToMethod.get(name), jb.getText());
+            }
+            bench.run();
+            
+            updateSeries(jb.getText());
+            chartPanel.repaint();
+        });
+        buttonsPanel.add(jb);
+    }
+
     private void updateSeries(String name) {
         double[][] results = loadResults(name);
         try {
